3823dab53edf4090001ba5d1af6102a10dc6a028
Rcurcurato <133296185+Rcurcurato@users.noreply.github.com>
2023-08-25 22:19:42 -0400

    added routes to server.js and added new index.js file under routes

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
-
-const PORT = 3001;
-
+const mainRouter = require('./routes/index.js')
 const app = express();
+const PORT = 3001;
 
 //middleware for parsing JSON data
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', mainRouter)
+
+app.listen (PORT, () => {
+    console.log(`app listening at http://localhost:${PORT}`)
+});
